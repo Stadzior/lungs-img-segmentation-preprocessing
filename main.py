07a4581d45ee8bad7049f3ee50135f2360f992dc
Stadzior <Stadzior@gmail.com>
2019-08-21 22:54:16 +0200

    Added more logic to defining ranges.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from logHelper import ExecuteWithLogs
-from preprocessingHelper import PerformPreprocessing, PerformPreprocessingForSingleFile
+from preprocessingHelper import PerformPreprocessing, PerformPreprocessingForSingleFile, CalculateMaskSizeLevels
 import os
 import sys
 import _thread
@@ -7,6 +7,7 @@ import _thread
 os.chdir("./data")
 log_file_path = "log.txt"
 files = list(filter(lambda x: x.endswith(".raw"), os.listdir(".")))  
-for file in files:
-    _thread.start_new_thread(ExecuteWithLogs("Preprocessing for {0}".format(file), log_file_path, lambda _ = None: PerformPreprocessingForSingleFile(file, log_file_path)))   
+#for file in files:
+    #_thread.start_new_thread(ExecuteWithLogs("Preprocessing for {0}".format(file), log_file_path, lambda _ = None: PerformPreprocessingForSingleFile(file, log_file_path)))   
+print(ExecuteWithLogs("Masks sizes calculation", log_file_path, lambda _ = None: CalculateMaskSizeLevels(20)))
 
--- preprocessingHelper.py
+++ preprocessingHelper.py
@@ -3,6 +3,8 @@ from PIL import Image
 from logHelper import ExecuteWithLogs
 import os
 
+TARGET_SIZE = (512, 512)
+
 def PerformPreprocessing(files, log_file_path): 
     for i, file in enumerate(files):
         print("{0}/{1} {2}".format(i, len(files), file))
@@ -15,7 +17,7 @@ def PerformPreprocessingForSingleFile(file, log_file_path):
     isMask = "MM" in file
 
     # converting to a int8/int16 numpy array
-    ct_image_as_vector = np.fromstring(img_str, dtype=np.int8) if isMask else np.fromstring(img_str, np.int16)
+    ct_image_as_vector = np.fromstring(img_str, dtype=np.dtype(bool)) if isMask else np.fromstring(img_str, dtype=np.int16)
 
     #layering
     layer_dimensions = (512, 512)
@@ -55,3 +57,25 @@ def PerformPreprocessingForSingleLayer(file, i, layer, isMask):
 def SliceListEvenly(source_list, slice_length):
     for i in range(0, len(source_list), slice_length):
         yield source_list[i:i + slice_length]
+
+def CalculateMaskSizeLevels(level_count):    
+    png_files = list(filter(lambda x: x.endswith(".png"), os.listdir(".")))
+    mask_sizes = []
+    for i, png_file in enumerate(png_files):
+        mask_size = CalculateMaskSize(png_file)
+        mask_sizes.append(mask_size)
+        print("{0}/{1} {2}".format(i+1, len(png_files), mask_size))
+    max_size = max(mask_sizes)
+    level_size = int(round(max_size/level_count))
+    levels = range(0, max_size, level_size)
+    levels_with_counts = []
+    for i in range(len(levels)-1):
+        levels_with_counts.append((len(list(filter(lambda x: levels[i] < x <= levels[i+1], mask_sizes))), levels[i]))
+    return levels_with_counts
+
+def CalculateMaskSize(png_file):
+    img = Image.open(png_file, mode='r') if png_file is not None else Image.new('L', TARGET_SIZE)
+    img_array = np.asarray(img)
+    unique, counts = np.unique(img_array, return_counts=True)
+    result = dict(zip(unique, counts))
+    return result[255] if 255 in result else 0
