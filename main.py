9a0224dd12a6dc3e65babee06ce58b90612b45c2
Stadzior <Stadzior@gmail.com>
2019-08-27 22:52:32 +0200

    fix fix

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import _thread
 os.chdir("./data")
 log_file_path = "log.txt"
 files = list(filter(lambda x: x.endswith(".raw"), os.listdir(".")))  
-#for file in files:
-    #_thread.start_new_thread(ExecuteWithLogs("Preprocessing for {0}".format(file), log_file_path, lambda _ = None: PerformPreprocessingForSingleFile(file, log_file_path)))   
-print(ExecuteWithLogs("Masks sizes calculation", log_file_path, lambda _ = None: CalculateMaskSizeLevels(20)))
+for file in files:
+    _thread.start_new_thread(ExecuteWithLogs("Preprocessing for {0}".format(file), log_file_path, lambda _ = None: PerformPreprocessingForSingleFile(file, log_file_path)))   
+#print(ExecuteWithLogs("Masks sizes calculation", log_file_path, lambda _ = None: CalculateMaskSizeLevels(20)))
 
